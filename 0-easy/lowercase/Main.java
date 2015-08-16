d46a80c33659fbf39b9c8321f9c2944f3dbade91
Kavit <kavitmht@gmail.com>
2015-08-16 17:59:35 -0400

    added lowercase code

---
+++ 0-easy/lowercase/Main.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args)throws Exception
+	{
+		File file = new File(args[0]);
+		BufferedReader in = new BufferedReader(new FileReader(file));
+		String line;
+
+		while((line=in.readLine()) != null)
+		{
+			line = line.trim();
+            System.out.println(line.toLowerCase());
+		}
+	}
+}
